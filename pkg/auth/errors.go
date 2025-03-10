4c530adaac94b1fd4c4fe58491a5a8727de14ff9
Michael Beutler <35310806+michaelbeutler@users.noreply.github.com>
2025-03-10 23:26:20 +0000

    removed unneeded error

--- pkg/auth/errors.go
+++ pkg/auth/errors.go
@@ -7,7 +7,6 @@ var (
 	ErrHTTPFailure          = errors.New("failed to connect to OAuth2 provider")
 	ErrInvalidResponse      = errors.New("received invalid response from OAuth2 provider")
 	ErrMissingResponseData  = errors.New("missing required data in the device authorization response")
-	ErrUnsupportedFlow      = errors.New("unsupported OAuth2 flow")
 	ErrAuthorizationPending = errors.New("authorization pending: user has not authorized yet")
 	ErrSlowDown             = errors.New("polling too frequently: slow down")
 	ErrTokenExpired         = errors.New("device code expired: user did not authorize in time")
